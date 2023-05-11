6d0a2e41f444f75d38c4e307db60f70df229535b
ChiaHsuan-Cheng <52044491+ChiaHsuan-Cheng@users.noreply.github.com>
2023-05-11 17:55:28 +0800

    add service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input, ViewChild, OnChanges } from '@angular/core';
+import { DataService } from './data.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'PJ-HrShow';
+
+
+  constructor(public DataSvc: DataService) {
+
+  }
+  ngOnInit(): void {
+    this.DataSvc.allHrdata(); // 呼叫在 service 建立的方法show
+  }
+
+
+
+
+
+
+
 }
+
